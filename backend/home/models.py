6a31444b535f783041c8fcb38157a3b05e906ac9
Crowdbotics <team@crowdbotics.com>
2021-07-20 23:44:53 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,18 @@
+from django.conf import settings
 from django.db import models
+
+
+class Input(models.Model):
+    "Generated Model"
+    coursename = models.CharField(
+        max_length=256,
+    )
+    courseCapacity = models.IntegerField(
+        null=True,
+        blank=True,
+    )
+    courseParticipant = models.ManyToManyField(
+        "users.User",
+        blank=True,
+        related_name="input_courseParticipant",
+    )
